c03ca98ae0cf25b0651b4c60149b18a60ed7c579
kingaspx <abnerodrigs@gmail.com>
2020-12-21 14:19:05 -0300

    video controls/autoplay added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react'
-import { Layout, Container, BoxUpload, ImagePreview } from './style';
-import FolderIcon from './assets/folder_icon_transparent.png'
-import CloseIcon from './assets/CloseIcon.svg'
+import React, { useState } from "react";
+import { Layout, Container, BoxUpload, ImagePreview } from "./style";
+import FolderIcon from "./assets/folder_icon_transparent.png";
+import CloseIcon from "./assets/CloseIcon.svg";
 
 function App() {
-  const [image, setImage] = useState('')
-  const [isUploaded, setIsUploaded] = useState(false)
-  const [typeFile, setTypeFile] = useState('')
+  const [image, setImage] = useState("");
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [typeFile, setTypeFile] = useState("");
 
   function handleImageChange(e) {
     if (e.target.files && e.target.files[0]) {
-      setTypeFile(e.target.files[0].type)
-      let reader = new FileReader()
+      setTypeFile(e.target.files[0].type);
+      let reader = new FileReader();
 
       reader.onload = function (e) {
-        setImage(e.target.result)
-        setIsUploaded(true)
-      }
+        setImage(e.target.result);
+        setIsUploaded(true);
+      };
 
-      reader.readAsDataURL(e.target.files[0])
+      reader.readAsDataURL(e.target.files[0]);
     }
   }
 
@@ -30,64 +30,71 @@ function App() {
 
         <BoxUpload>
           <div className="image-upload">
-            {
-              !isUploaded ? (
-                <>
-                  <label htmlFor="upload-input">
-                    <img src={FolderIcon} draggable={'false'} alt="placeholder" style={{ width: 100, height: 100 }} />
-                    <p style={{ color: '#444' }}>
-                      Click to upload image
-                    </p>
-                  </label>
-
-                  <input
-                    id="upload-input"
-                    type="file"
-                    accept=".jpg,.jpeg,.gif,.png,.mov,.mp4"
-                    onChange={handleImageChange}
+            {!isUploaded ? (
+              <>
+                <label htmlFor="upload-input">
+                  <img
+                    src={FolderIcon}
+                    draggable={"false"}
+                    alt="placeholder"
+                    style={{ width: 100, height: 100 }}
                   />
-                </>
-              ) : (
-                  <ImagePreview>
-                    <img
-                      className="close-icon"
-                      src={CloseIcon}
-                      alt="CloseIcon"
-                      onClick={() => {
-                        setIsUploaded(false)
-                        setImage(null)
-                      }}
-                    />
-                    <img
-                      id="uploaded-image"
-                      src={image}
-                      draggable={false}
-                      alt="uploaded-img"
-                    />
-                  </ImagePreview>
-                )
-            }
+                  <p style={{ color: "#444" }}>Click to upload image</p>
+                </label>
+
+                <input
+                  id="upload-input"
+                  type="file"
+                  accept=".jpg,.jpeg,.gif,.png,.mov,.mp4"
+                  onChange={handleImageChange}
+                />
+              </>
+            ) : (
+              <ImagePreview>
+                <img
+                  className="close-icon"
+                  src={CloseIcon}
+                  alt="CloseIcon"
+                  onClick={() => {
+                    setIsUploaded(false);
+                    setImage(null);
+                  }}
+                />
+                {typeFile.includes("video") ? (
+                  <video
+                    id="uploaded-image"
+                    src={image}
+                    draggable={false}
+                    controls
+                    autoPlay
+                    alt="uploaded-img"
+                  />
+                ) : (
+                  <img
+                    id="uploaded-image"
+                    src={image}
+                    draggable={false}
+                    alt="uploaded-img"
+                  />
+                )}
+              </ImagePreview>
+            )}
           </div>
         </BoxUpload>
 
-        {
-          isUploaded ? (
-            <h2>
-              Type is {typeFile}
-            </h2>
-          ) : null
-        }
+        {isUploaded ? <h2>Type is {typeFile}</h2> : null}
 
-        <h3>
-          Source Code:
-        </h3>
-        <a href="https://github.com/kingaspx/ImagePreview-React" rel="noreferrer" target={'_blank'} style={{ marginTop: -10 }}>
+        <h3>Source Code:</h3>
+        <a
+          href="https://github.com/kingaspx/ImagePreview-React"
+          rel="noreferrer"
+          target={"_blank"}
+          style={{ marginTop: -10 }}
+        >
           Available on <b>Github</b>
         </a>
 
-        <h3>
-          By Kingaspx
-        </h3>
+        <h3>By Kingaspx</h3>
       </Container>
     </Layout>
   );
